e321af6f30492141d922add0145b4aea66b67cc9
simonegreco5 <simoxey.greco@gmail.com>
2026-03-28 19:26:59 +0100

    add solution in snack 3

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -2,3 +2,25 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+
+// creiamo un nuovo array con incremento di 1, utilizzando il metodo .map
+const moreNumbers = numbers.map((number)=>{
+    return number + 1 
+})
+console.log(moreNumbers);
+
+// facciamo lo stesso con ciclo for, questa volta ci servirà una variabile d'appoggio
+const increseNum = []
+for (let i = 0; i < numbers.length; i++){
+    increseNum.push(numbers[i] + 1)
+}
+console.log(increseNum);
+
+// lo stesso con ciclo forEach, sempre con una variabile d'appoggio
+const increseNumb = []
+numbers.forEach((number)=>{
+    increseNumb.push(number + 1)
+})
+console.log(increseNumb);
+
