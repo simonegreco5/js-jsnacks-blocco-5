3f4f7a6c0b80b7fe40f4fc55a447ee4e3b2b2204
simonegreco5 <simoxey.greco@gmail.com>
2026-03-28 19:05:05 +0100

    add solutions in snack 1

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -2,3 +2,13 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+// stampa con ciclo for
+for (let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
+
+// stampa con ciclo forEach
+names.forEach((name)=>{
+    console.log(name)
+})
