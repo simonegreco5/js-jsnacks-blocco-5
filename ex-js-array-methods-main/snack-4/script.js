6d6154f8c7c023405c7859d12e7cf5c6f1a8e7c9
simonegreco5 <simoxey.greco@gmail.com>
2026-03-28 19:37:24 +0100

    add solutions in snack 4

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -7,3 +7,23 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+// stampiamo con il metodo .map
+const autori = posts.map((name)=>{
+  return name.author
+})
+console.log(autori);
+
+// stampiamo con il ciclo forEach, servendoci di una variabile d'appoggio 
+const scrittori = []
+posts.forEach((name)=>{
+  scrittori.push(name.author)
+})
+console.log(scrittori);
+
+// facciamo lo stesso utilizzando un classico ciclo for, sempre con variabile d'appoggio 
+const ideatori = []
+for (let i = 0; i < posts.length; i++){
+  ideatori.push(posts[i].author)
+}
+console.log(ideatori)
