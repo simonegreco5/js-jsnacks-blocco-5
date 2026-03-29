c219da475a51b0e47226937d9f855fc171b2b798
simonegreco5 <simoxey.greco@gmail.com>
2026-03-29 20:48:37 +0200

    add solutions in snack 8 (review first solution)

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -21,3 +21,30 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+// metodo non corretto (DA RIVEDERE)
+const classRoom = students.find((student)=>{
+  return student.class === '3C'
+})
+console.log(classRoom)
+
+// per stampare correttamente il valore 3C in console utilizziamo il metodo forEach, denominado una variabile
+// e assegnandola poi nel condizionale all'interno del forEach
+let classroom;
+students.forEach((student)=>{
+  if (student.class === '3C'){
+    classroom = '3C'
+  }
+})
+console.log(classroom)
+
+// facciamo lo stesso procedimento del metodo forEach con un ciclo for classico
+let classe;
+for (let i = 0; i < students.length; i++){
+  let student = students[i]
+
+  if (student.class === '3C'){
+    classe = '3C'
+  }
+}
+console.log(classe)
