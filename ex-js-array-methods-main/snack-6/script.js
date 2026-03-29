cb6d0053192227f7cf4a3da014d7fb2090078c21
simonegreco5 <simoxey.greco@gmail.com>
2026-03-29 20:12:19 +0200

    add solutions in snack 6

--- ex-js-array-methods-main/snack-6/script.js
+++ ex-js-array-methods-main/snack-6/script.js
@@ -12,3 +12,44 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+// per creare le due array adoperiamo il metodo .filter
+// array (longer) zucchine lunghe >= 15
+const longer = zucchine.filter((zucchina)=>{
+  return zucchina.length >= 15
+})
+
+// array (shorter) zucchine lunghe < 15
+const shorter = zucchine.filter((zucchina)=>{
+  return zucchina.length < 15
+})
+console.log(longer, shorter)
+
+// facciamo lo stesso utilizzando il metodo forEach, servendoci di due array di appoggio
+const long = []; const short = [];
+
+zucchine.forEach((zucchina)=>{
+
+  if (zucchina.length >= 15 ){
+    long.push(zucchina)
+
+  } else {
+    short.push(zucchina)
+  }
+})
+console.log(long, short)
+
+// facciamo la stessa cosa utilizzando un ciclo for
+const longs = []; const shorts = [];
+
+for (let i = 0; i < zucchine.length; i++){
+  let zucchina = zucchine[i]
+
+  if (zucchina.length >= 15){ // oppure, senza variabile (zucchina[i].length ...) 
+    longs.push(zucchina)
+
+  } else {
+    shorts.push(zucchina)
+  }
+}
+console.log(longs, shorts)
