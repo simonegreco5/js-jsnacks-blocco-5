3862e2745a36dd3070c4c9d78d70110cabdef6ed
simonegreco5 <simoxey.greco@gmail.com>
2026-03-29 20:25:09 +0200

    add solutions in snack 7

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -21,3 +21,27 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+// utilizziamo il metodo .find per trovare la persona con l'id: 2
+const person = students.find((student)=>{
+  return student.id === 2
+})
+console.log(person);
+
+// facciamo la stessa cosa con il metodo forEach, utilizzando un array di appoggio
+const persona = []
+students.forEach((student)=>{
+  if (student.id === 2){
+    persona.push(student)
+  }
+})
+console.log(persona)
+
+// facciamo lo stesso con il ciclo for
+const alunno = []
+for (let i = 0; i < students.length; i++){
+  if (students[i].id === 2){
+    alunno.push(students[i])
+  }
+}
+console.log(alunno)
